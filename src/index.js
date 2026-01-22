eeae52e4cb50c0ca3ab67d5a9ba0c6f37e3a39c0
Rudraksh Upadhyay <218912408+rudrakshupadhyay@users.noreply.github.com>
2026-01-22 15:22:07 +0530

    add basic structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+import cupLogo from "./cuplogo.svg";
 import "./styles.css";
+
+const logo = document.querySelector(".logo");
+console.log(logo);
+const Image = document.createElement("img");
+Image.src = cupLogo;
+Image.classList.add("logoPhoto");
+logo.appendChild(Image);
 console.log("hello bro");
