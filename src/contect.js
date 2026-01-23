544a7f4076f4ae2f128ba28cf70d0c39ffab61d4
rudrakshupadhyay <218912408+rudrakshupadhyay@user.noreply.github.com>
2026-01-23 16:24:35 +0530

    add contect up button function

---
+++ src/contect.js
@@ -0,0 +1,27 @@
+export default function loadContect(){
+    const content = document.getElementById("content");
+    const box = document.createElement("div");
+    box.classList.add("box");
+    // menuGroup
+    const tagLine = document.createElement("div");
+    tagLine.textContent = "Contact Us";
+    tagLine.classList.add("tagLine");
+    const menuGroup = document.createElement("div");
+    menuGroup.classList.add("menuGroup");
+    menuGroup.innerHTML = `
+                        <div class="item">
+                            <div class="contItemTop">Mama Owl</div>
+                            <div class="contCheif">Chef<br>555-555-5554<br>totallyRealEmail@notFake.com</div>
+                        </div>
+                        <div class="item">
+                            <div class="contItemTop">Papa Owl</div>
+                            <div class="contCheif">Manager<br>555-555-5555<br>perfectlyRealEmail@notFake.com</div>
+                        </div>
+                        <div class="item">
+                            <div class="contItemTop">Baby Owl</div>
+                            <div class="contCheif">Waiter<br>555-555-5556<br>totallyRealEmail@notFake.com</div>
+                        </div>
+    `;
+    box.append(tagLine,menuGroup);
+    content.append(box);
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import loadHome from "./home";
 import loadMenu from "./menu";
+import loadContect from "./contect";
 import cupLogo from "./cuplogo.svg";
 import "./styles.css";
 
-const logo = document.querySelector(".logo");
-console.log(logo);
-const Image = document.createElement("img");
-Image.src = cupLogo;
-Image.classList.add("logoPhoto");
-logo.appendChild(Image);
+(() => { 
+    const logo = document.querySelector(".logo");
+    console.log(logo);
+    const Image = document.createElement("img");
+    Image.src = cupLogo;
+    Image.classList.add("logoPhoto");
+    logo.appendChild(Image)})();
+
 console.log("hello bro");
-let currTab = "none";
 const home = document.getElementById("home");
+const menu = document.getElementById("menu");
+const contact = document.getElementById("contact");
+let currTab = "none";
+const bnt = [home,menu,contact];
 home.addEventListener("click", () => {
     if(currTab === "home") return;
-    loadMenu();
+    loadContect();
     currTab = "home";
 });
    
